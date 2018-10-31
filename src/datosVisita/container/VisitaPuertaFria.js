b7de5172d0f7f020afd07096419e57f4da6664c3
Carlos Raez <carlosraez@gmail.com>
2018-10-31 11:01:31 +0100

    creado componente VisitaPuertaFria a falta del boton volver a dash

--- src/datosVisita/container/VisitaPuertaFria.js
+++ src/datosVisita/container/VisitaPuertaFria.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import Visita from './visita.js'
 import { firebaseApp } from '../../index.js'
 import swal from 'sweetalert';
+import DatosVisita from '../components/datosVisita.js'
+import SmartAscensor from '../components/smartAscensor.js'
+import TerminarVisita from '../components/terminarVisita.js'
+import VisitaLayout from '../components/visitaLayout.js'
 
 
 
@@ -32,6 +35,51 @@ export class VisitaPuertaFria extends Component {
       visita:[],
       embarques:'',
       observacionAscensorSinAscensor:'',
+      mostrarDatosVisita:true,
+      mostradDatosAscensor: true,
+  }
+
+
+  handleReturnDatosVisita = () => {
+    this.setState({
+      mostrarDatosVisita:true
+    })
+
+  }
+
+  handleReturnDatosAscensor = () => {
+    this.setState({
+     mostradDatosAscensor : true,
+    })
+  }
+
+  handleClickContinuar = () => {
+    swal({
+     title: "¿Has terminado de rellenar los datos?",
+     text: "Por favor rellena los datos del ascensor o los de Finca Sin Ascensor",
+    icon: "warning",
+   buttons: true,
+   dangerMode: true,
+  })
+  .then((escritoAscensores) => {
+   if (escritoAscensores && this.props.cantidadAscensoresGuardados > 0) {
+     this.setState({
+       mostradDatosAscensor: false,
+     })
+     swal("Muy Bien!! ya queda poco", {
+       icon: "success",
+     });
+   } else {
+      swal('Me lo imaginaba, no has terminado...Por favor termina de rellenar los datos... o Pulsa en Edificio sin Ascensor')
+   }
+     });
+
+  }
+
+  handleClickDatosVisita = () =>  {
+     this.setState({
+       mostrarDatosVisita:false
+     })
   }
 
   handleClickTerminarVisita = () => {
@@ -144,16 +192,37 @@ export class VisitaPuertaFria extends Component {
 
   render() {
      return (
-       <Visita
-       handleChangeUpload={this.handleChangeUpload}
-       imagenesAscensor={this.state.imagenesAscensor}
-       handleClickfincaSinAscensor={this.handleClickfincaSinAscensor}
-       handleClickTerminarVisita={this.handleClickTerminarVisita}
-       handleChange={this.handleChange}
-       handleClickSiguienteAscensor={this.handleClickSiguienteAscensor}
-       cantidadAscensoresGuardados={this.state.ascensor.length}
-       handleClickDash={this.props.handleClickDash}
-         />
+       <VisitaLayout
+        handleClickDash={this.props.handleClickDash}
+       >
+           {
+           this.state.mostrarDatosVisita ?
+           <DatosVisita
+           handleClickDatosVisita={this.handleClickDatosVisita}
+           handleChange={this.handleChange}
+           checked={this.state.checked}
+           handleClickDash={this.props.handleClickDash}
+           />
+           :
+           this.state.mostradDatosAscensor ?
+           <SmartAscensor
+              handleChangeUpload={this.handleChangeUpload}
+              imagenesAscensor={this.state.imagenesAscensor}
+              cantidadDeAscensoresIncluidos={this.cantidadAscensoresGuardados}
+              handleClickSiguienteAscensor={this.handleClickSiguienteAscensor}
+              handleReturnDatosVisita={this.handleReturnDatosVisita}
+              handleChange={this.handleChange }
+              handleClickContinuarYTerminar={this.handleClickContinuar}
+              handleClickDash={this.props.handleClickDash}
+             />
+            :
+              <TerminarVisita
+               handleChange={this.handleChange }
+               guardarYTerminar={this.handleClickTerminarVisita}
+               handleClickDash={this.props.handleClickDash}
+               />
+           }
+     </VisitaLayout>
      )
   }
 }
