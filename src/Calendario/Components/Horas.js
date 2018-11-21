18d64161fc9494c960e3afaa9019b7e77850bfaf
Carlos Raez <carlosraez@gmail.com>
2018-11-21 22:38:25 +0100

    atascado en calendario

--- src/Calendario/Components/Horas.js
+++ src/Calendario/Components/Horas.js
@@ -5,7 +5,7 @@ function Horas(props) {
    return (
           <tr>
             <th scope="row">{props.hora}</th>
-            <td id={props.lunes} name={props.hora} className={props.estilo}><button id={props.lunes} name={props.hora} onClick={props.handleClickLibreLunes} className="btn btn-link btn-block">{props.textoBoton}</button></td>
+            <td id={props.hora}  className={props.estilo}><button id={props.hora} onClick={props.handleClickLibreLunes} className="btn btn-link btn-block">{props.textoBoton}</button></td>
             <td className="libre"><button id={props.id} onClick={props.handleClickLibreMartes} className="btn btn-link btn-block">Reservar</button></td>
             <td className="libre"><button id={props.id} onClick={props.handleClickLibreMiercoles} className="btn btn-link btn-block">Reservar</button></td>
             <td className="libre"><button id={props.id} onClick={props.handleClickLibreJueves} className="btn btn-link btn-block">Reservar</button></td>
--- src/Calendario/Container/CalendarioAgenda.js
+++ src/Calendario/Container/CalendarioAgenda.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import AgendaComponentLayaout from '../Components/AgendaComponentLayout.js'
-import DiaSemana from '../Components/DiaSemana.js'
 import Horas from '../Components/Horas.js'
-import DiaReserva from '../Components/DiaReserva.js'
 import moment from 'moment'
 import '../../../src/locale.js'
 
@@ -51,10 +49,8 @@ export class CalendarioAgenda extends Component {
    handleClickLibreLunes = (event) => {
      const target = event.target
      const  id = target.id
-     const name = target.name
-     this.setState({
+      this.setState({
        id:id,
-       name:name
      })
    }
 
@@ -65,38 +61,33 @@ export class CalendarioAgenda extends Component {
      semana.push(dia)
      }
      const horas = [
-     '8:00','8:15','8:30','8:45','9:00','9:15','9:30','9:45','10:00','10:15','10:30','10,45','11:00','11:15','11:30','11:45','12:00','12:15','12:30','12:45','13:00','13:15','13:30'
+     '8:00','8:15','8:30','8:45','9:00','9:15','9:30','9:45','10:00','10:15','10:30','10:45','11:00','11:15','11:30','11:45','12:00','12:15','12:30','12:45','13:00','13:15','13:30'
      ,'13:45','14:00','14:15','14:30','14:45','15:00','15:15','15:30','15:45','16:00','16:15','16:30','16:45','17:00','17:15','17:30','17:45','18:00','18:15','18:30','18:45',
     '19:00','19:15','19:30','19:45','20:00','20:15','20:30','20:45','21:00']
      return (
        <table className="table table-bordered table-sm table-striped">
         <thead>
            <tr>
-         <th scope="col">Hora</th>
+        <th scope="col">Hora</th>
         <th scope="col">Lunes: {semana[1]}</th>
         <th scope="col">Martes: {semana[2]}</th>
         <th scope="col">Miercoles: {semana[3]}</th>
         <th scope="col">Jueves: {semana[4]}</th>
         <th scope="col">Viernes: {semana[5]}</th>
         <th scope="col">Sabado: {semana[6]}</th>
-        </tr>
+         </tr>
           </thead>
        <tbody>
       {
         horas.map((hora) => {
           return (
            <Horas
-          key={hora}
-          lunes={semana[1]}
-          martes={semana[2]}
-          miercoles={semana[3]}
-          jueves={semana[4]}
-          viernes={semana[5]}
-          sabado={semana[6]}
-          hora={hora}
-          handleClickLibreLunes={this.handleClickLibreLunes}
-          textoBoton={this.state.textoBoton}
-          estilo={this.state.estilo}
+           lunes={semana[1]}
+           id={hora}
+           key={hora}
+           hora={hora}
+           textoBoton={this.state.textoBoton}
+           handleClickLibreLunes={this.handleClickLibreLunes}
            />
           )
         })
