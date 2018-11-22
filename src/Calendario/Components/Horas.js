bde6cb14351c9ac5bb434a970528d148cef0ba13
Carlos Raez <carlosraez@gmail.com>
2018-11-22 21:49:25 +0100

    ya guardamos el mes correspondiente

--- src/Calendario/Components/Horas.js
+++ src/Calendario/Components/Horas.js
@@ -9,10 +9,10 @@ export class Horas extends Component {
    }
 
 
-  handleClickReserva = (event,dia,hora) => {
+  handleClickReserva = (event,dia,hora,mes) => {
    const { reservados } =  this.state
    let reserva = reservados
-   reserva.push(`${dia} ${hora}`)
+   reserva.push(`${dia} ${mes} ${hora}`)
    this.setState({
     reservados: reserva,
     textoBoton: 'Ocupado'
@@ -21,12 +21,14 @@ export class Horas extends Component {
 
    render() {
      const { textoBoton, reservados }  = this.state
-     const { hora , semana } = this.props
+     const { hora , semana, mes } = this.props
+     console.log(reservados);
+     console.log(reservados.indexOf(`${semana[1]} ${hora}`));
      return (
        <tr>
          <th scope="row">{hora}</th>
-         <td ref={(ref) => { this.referenciaPrueba = ref }} className={reservados.indexOf(`${semana[1]} ${hora}`) > - 1 ?  'ocupado'  : 'libre' }><button
-         onClick={(event) => { this.handleClickReserva(event,this.props.semana[1],this.props.hora) }} className="btn btn-link btn-block">{textoBoton}</button></td>
+         <td ref={(ref) => { this.referenciaPrueba = ref }} className={reservados.indexOf(`${semana[1]} ${mes[1]} ${hora}`) > - 1 ?  'ocupado'  : 'libre' }><button
+         onClick={(event) => { this.handleClickReserva(event,semana[1],hora,mes[1]) }} className="btn btn-link btn-block">{textoBoton}</button></td>
 
      </tr>
      )
