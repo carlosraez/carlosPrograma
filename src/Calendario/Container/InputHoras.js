32467064f45cfeea375c758d050a9de6e98a2f71
Carlos Raez <carlosraez@gmail.com>
2018-12-06 12:08:58 +0100

    he añadido el estilo en linea con una constante y defiendo el style

--- src/Calendario/Container/InputHoras.js
+++ src/Calendario/Container/InputHoras.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import './InputReserva.css'
 
+
+  
+
 export class Inputhoras extends Component {
  
-    
+ divStyle() {
+   const tiempo  =  33  * 4
+
+     return   {
+        borderWidth: 1,
+        border: 'solid',
+        width:  170,
+        borderRadius: 5,
+        height: tiempo,
+        position: 'absolute',
+        marginLeft: -5,
+        marginTop: -5,
+        backgroundColor: '#17a2b8',
+        fontSize: 12,
+     }
+ }
 
    render() {
 
@@ -11,7 +29,7 @@ export class Inputhoras extends Component {
 
      
      return (
-    <div className="reservaBorder">
+    <div style={this.divStyle()}>
     <h5>{`${fechaInicioReunion} - ${fechaFinalReunion}`}</h5>
     <p>{tituloReservaBaseDatos}</p>    
     </div>
