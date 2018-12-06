ae3c8e67f94b0817ce7846961c66cb706a097f82
Carlos Raez <carlosraez@gmail.com>
2018-12-06 19:50:14 +0100

    ya hace estirar para abajo la reserva del calendario

--- src/Calendario/Container/InputHoras.js
+++ src/Calendario/Container/InputHoras.js
@@ -1,40 +1,38 @@
 import React, { Component } from 'react';
+import { Rnd }  from 'react-rnd'
 import './InputReserva.css'
 
 
   
 
 export class Inputhoras extends Component {
- 
- divStyle() {
-   const tiempo  =   parseInt(this.props.minutosTotales,10) * 3.3
-     return   {
-        borderWidth: 1,
-        border: 'solid',
-        borderColor:  '#212529',
-        textAlign:'center',
-        width:  170,
-        borderRadius: 5,
-        height: tiempo,
-        position: 'absolute',
-        marginLeft: -5,
-        marginTop: -5,
-        backgroundColor: '#17a2b8',
-        fontSize: 12,
-        color: '#fff'
-     }
- }
+   state = {
+       height:parseInt(this.props.minutosTotales,10) * 3.3,
+       
+   }
 
    render() {
 
     const { fechaInicioReunion ,fechaFinalReunion, tituloReservaBaseDatos,  } = this.props
-
-     
+    
      return (
-    <div style={this.divStyle()}>
+    <Rnd
+    enableResizing={{bottom:true}}
+    disableDragging={true}
+    size={{ height: this.state.height, width:170 }}
+    minHeight={66}
+    className='reservaBorder' 
+    onResizeStop={(e, direction, ref, delta, position) => {
+        console.log('h');
+        
+        this.setState({
+            height: ref.style.height,
+        });
+      }}
+    >
     <h5>{`${fechaInicioReunion} - ${fechaFinalReunion}`}</h5>
     <p>{tituloReservaBaseDatos}</p>    
-    </div>
+    </Rnd>
      )
    }
 }
